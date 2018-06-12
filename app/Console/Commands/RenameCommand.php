411e02097165d5380c0c4144ecade6c1a2bb9d30
Rick Selby <rick@selby-family.co.uk>
2018-06-12 09:19:49 +0100

    Add rename command

---
+++ app/Console/Commands/RenameCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\RenameService;
+use Illuminate\Console\Command;
+
+class RenameCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $signature = "rename";
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "Rename music files";
+
+    /** @var RenameService */
+    private $renameService;
+
+    public function __construct(RenameService $renameService)
+    {
+        parent::__construct();
+        $this->renameService = $renameService;
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->renameService->rename();
+    }
+}
---
+++ app/Services/RenameService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+class RenameService
+{
+    public function rename()
+    {
+        // TODO
+    }
+}
