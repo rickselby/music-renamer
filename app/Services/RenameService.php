85132e7ce657b0221f922f78c41246ea5b5aaebb
Rick Selby <rick@selby-family.co.uk>
2018-06-13 14:03:15 +0100

    Actually move files!

--- app/Services/RenameService.php
+++ app/Services/RenameService.php
@@ -12,6 +12,9 @@ class RenameService
     /** @var Filesystem */
     protected $source;
 
+    /** @var Filesystem */
+    protected $destination;
+
     /** @var getID3 */
     protected $id3;
 
@@ -21,11 +24,12 @@ class RenameService
     public function __construct(getID3 $getID3, VerifyService $verifyService)
     {
         $this->source = \Storage::disk('source');
+        $this->destination = \Storage::disk('destination');
         $this->id3 = $getID3;
         $this->verifyService = $verifyService;
     }
 
-    public function rename(Command $command)
+    public function renameAndVerify(Command $command)
     {
         $this->parseDirectory('/', $command);
     }
@@ -45,7 +49,8 @@ class RenameService
             $tags = $this->getTags($directory);
 
             if ($this->verifyService->verify($tags)) {
-                // Move the files to the destination
+                $command->info('Moving "'.$directory.'"');
+                $this->moveFiles($directory, $tags);
             } else {
                 $command->error('Could not move "'.$directory.'"');
                 $this->verifyService->getErrors()->each(function ($error) use ($command) {
@@ -74,5 +79,64 @@ class RenameService
             );
         }
         return $tags;
+    }
+
+    protected function moveFiles(string $directory, Collection $files)
+    {
+        /* ArtistName/AlbumName/TrackNumber - TrackName.ext */
+        /* ArtistName/AlbumName/DiscNumber - TrackNumber - TrackName.ext */
+
+        $discNumbers = ($files->pluck('part_of_a_set')->unique()->count() != 1);
+
+        $files->each(function (Collection $tags, $file) use ($directory, $discNumbers) {
+            $sourcePath = $directory . DIRECTORY_SEPARATOR . $file;
+
+            if ($discNumbers) {
+                $destinationPath =
+                    $tags->get('band')[0]
+                    . DIRECTORY_SEPARATOR
+                    . $tags->get('album')[0]
+                    . DIRECTORY_SEPARATOR
+                    . $this->getDiscNumber($tags)
+                    .' - '
+                    . $this->getTrackNumber($tags)
+                    . ' - '
+                    . $tags->get('artist')[0]
+                    . ' - '
+                    . $tags->get('title')[0];
+
+            } else {
+                $destinationPath =
+                    $tags->get('artist')[0]
+                    . DIRECTORY_SEPARATOR
+                    . $tags->get('album')[0]
+                    . DIRECTORY_SEPARATOR
+                    . $this->getTrackNumber($tags)
+                    . ' - '
+                    . $tags->get('title')[0];
+            }
+
+            // Copy the file
+            $this->destination->put($destinationPath, $this->source->readStream($sourcePath));
+            // Remove the source
+            $this->source->delete($sourcePath);
+        });
+
+        $this->source->deleteDir($directory);
+    }
+
+    protected function getDiscNumber(Collection $tags)
+    {
+        return explode('/', $tags->get('part_of_a_set')[0])[0];
+    }
+
+    protected function getTrackNumber(Collection $tags)
+    {
+        return str_pad(
+            explode('/', $tags->get('track_number')[0])[0],
+            2,
+            '0',
+            STR_PAD_LEFT
+        );
     }
 }
